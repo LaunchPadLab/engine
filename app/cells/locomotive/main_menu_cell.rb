3b51028417689ec328282d5a1ae7da28cc0e5081
Ryan Francis <ryan.p.francis@gmail.com>
2014-10-20 16:20:27 -0500

    active styling working on super admin tab

--- app/cells/locomotive/main_menu_cell.rb
+++ app/cells/locomotive/main_menu_cell.rb
@@ -4,9 +4,9 @@ module Locomotive
     protected
 
     def build_list
-      add :contents,             url: pages_path,             icon: 'icon-folder-open'
-      add :settings,             url: edit_current_site_path, icon: 'icon-cog'
-      add :shared_resources,     url: shared_resources_path,  icon: 'icon-cog' if can?(:manage, SharedResource)
+      add :contents,      url: pages_path,             icon: 'icon-folder-open'
+      add :settings,      url: edit_current_site_path, icon: 'icon-cog'
+      add :super_admin,   url: shared_resources_path,  icon: 'icon-cog' if can?(:manage, SharedResource)
     end
 
   end

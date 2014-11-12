66ba4028a8babdcb2f52c5e46ea157ea3d912bce
Ryan Francis <ryan.p.francis@gmail.com>
2014-11-12 11:16:27 -0600

    refactored custom api logic into separate class

--- app/controllers/locomotive/api/content_entries_controller.rb
+++ app/controllers/locomotive/api/content_entries_controller.rb
@@ -11,24 +11,7 @@ module Locomotive
 
       def index
         @content_entries = @content_entries.order_by([get_content_type.order_by_definition])
-
-        if params[:start_date] && params[:end_date]
-          start_date = Date.parse(params[:start_date])
-          end_date = Date.parse(params[:end_date])
-          @content_entries = @content_entries.where(:start_time.gte => start_date, :end_time.lte => end_date)
-        end
-
-        if params[:function_id].present?
-          function_entry = function_content_type.entries.find(params[:function_id])
-          group_ids = group_content_type.entries.where(function: function_entry).map(&:_id)
-          @content_entries = @content_entries.where(:group_id.in => group_ids)
-        end
-
-        if params[:group_id].present?
-          group_entry = group_content_type.entries.find(params[:group_id])
-          @content_entries = @content_entries.where(group: group_entry)
-        end
-
+        @content_entries = Locomotive::Meritas::Api::ContentEntry.new(content_entries: @content_entries, params: params, content_type: @content_type, site: current_site).entries
         respond_with @content_entries
       end
 
@@ -59,13 +42,7 @@ module Locomotive
         @content_type ||= current_site.content_types.where(slug: params[:slug]).first
       end
 
-      def group_content_type
-        @group_content_type ||= current_site.content_types.where(slug: "group").first
-      end
 
-      def function_content_type
-        @function_content_type ||= current_site.content_types.where(slug: "function").first
-      end
     end
   end
 end
---
+++ app/models/locomotive/meritas/api/content_entry.rb
@@ -0,0 +1,75 @@
+module Locomotive
+  class Meritas::Api::ContentEntry
+
+    attr_reader :params, :content_type, :site
+    attr_accessor :content_entries
+
+    MERITAS_CUSTOM_CONTENT_TYPES = ["event"]
+
+    def initialize(args = {})
+      @content_entries = args[:content_entries]
+      @params = args[:params]
+      @content_type = args[:content_type]
+      @site = args[:site]
+    end
+
+    def entries
+      return @content_entries unless MERITAS_CUSTOM_CONTENT_TYPES.include?(slug)
+      send("#{slug}_entries")
+    end
+
+    private
+
+      def slug
+        @content_type.slug.downcase
+      end
+
+      def event_entries
+        filter_by_date_range if start_date.present? && end_date.present?
+        filter_by_function if params[:function_id]
+        filter_by_group if params[:group_id]
+      end
+
+      def start_date
+        @start_date ||= params[:start_date]
+      end
+
+      def end_date
+        @end_date ||= params[:start_date]
+      end
+
+      def filter_by_date_range
+        start_date = Date.parse(params[:start_date])
+        end_date = Date.parse(params[:end_date])
+        @content_entries = @content_entries.where(:start_time.gte => start_date, :end_time.lte => end_date)
+      end
+
+      # GROUP
+      def filter_by_group
+        @content_entries = @content_entries.where(group: group_entry)
+      end
+
+      def group_content_type
+        @group_content_type ||= @site.content_types.where(slug: "group").first
+      end
+
+      def group_entry
+        group_content_type.entries.find(params[:group_id])
+      end
+
+      # FUNCTION
+      def filter_by_function
+        group_ids = group_content_type.entries.where(function: function_entry).map(&:_id)
+        @content_entries = @content_entries.where(:group_id.in => group_ids)
+      end
+
+      def function_content_type
+        @function_content_type ||= @site.content_types.where(slug: "function").first
+      end
+
+      def function_entry
+        function_content_type.entries.find(params[:function_id])
+      end
+
+  end
+end

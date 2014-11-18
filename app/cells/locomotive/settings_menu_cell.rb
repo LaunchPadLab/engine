0f1fbbccd0c063571b1e534617b31dbc1fea4404
Ryan Francis <ryan.p.francis@gmail.com>
2014-11-18 16:58:44 -0600

    fixed slight bug on settings menu

--- app/cells/locomotive/settings_menu_cell.rb
+++ app/cells/locomotive/settings_menu_cell.rb
@@ -8,8 +8,8 @@ module Locomotive
       add :theme_assets,          url: theme_assets_path
       add :translations,          url: translations_path
       add :account,               url: edit_my_account_path
-      add :approvals,             url: previews_pages_path, if can?(:manage, Page)
-      add :public_resources,      url: public_resources_path, if can?(:edit, Page)
+      add :approvals,             url: previews_pages_path if can?(:manage, Page)
+      add :public_resources,      url: public_resources_path if can?(:edit, Page)
     end
 
   end

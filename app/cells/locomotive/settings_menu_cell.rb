efd8d4fb1cba5c5ce586c98e62ed33338c671b1e
Ryan Francis <ryan.p.francis@gmail.com>
2014-12-01 15:27:29 -0600

    added ability to import events and refactored import into module

--- app/cells/locomotive/settings_menu_cell.rb
+++ app/cells/locomotive/settings_menu_cell.rb
@@ -10,6 +10,7 @@ module Locomotive
       add :account,               url: edit_my_account_path
       add :approvals,             url: previews_pages_path if can?(:manage, Page)
       add :public_resources,      url: public_resources_path if can?(:edit, Page)
+      add :import_events,         url: new_import_event_path if can?(:edit, Page)
     end
 
   end

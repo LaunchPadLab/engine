368128316b4b3f8e5b0d5af3f571dbfdcd6f4dd3
Ryan Francis <ryan.p.francis@gmail.com>
2015-02-10 23:55:38 -0600

    moved meritas specific files to meritas engine

--- app/models/locomotive/meritas/api/event_series/frequency/weekly.rb
+++
@@ -1,9 +0,0 @@
-module Locomotive
-  class Meritas::Api::EventSeries::Frequency::Weekly < Meritas::Api::EventSeries::Frequency
-
-    def dates_from_frequency
-      weekly_dates(1)
-    end
-
-  end
-end
